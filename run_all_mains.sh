7a44863bc8c329cc1038e56c5549bd954152e2c1
Richard Anton <richard.anton@snowflake.com>
2024-04-19 11:44:33 -0700

    Update run all script to skip streamlit app

--- run_all_mains.sh
+++ run_all_mains.sh
@@ -2,9 +2,13 @@
 
 set -e 
 
-FILES_WITH_MAIN=`grep -l main *.py`
+FILES_WITH_MAIN=`grep -l main *.py | grep -v streamlit_app`
 for F in $FILES_WITH_MAIN; do
     echo "Running $F"
     python $F
+    echo "DONE: $F"
     echo
 done
+
+echo "Finished."
+
